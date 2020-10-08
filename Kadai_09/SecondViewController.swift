f1add3f9cf672c19ebe6e800f119093b3816f0f9
kodou <kodou.rhythm@gmail.com>
2020-10-09 08:38:41 +0900

    処理追加

---
+++ Kadai_09/SecondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SecondViewController.swift
+//  Kadai_09
+//
+//  Created by kodou on 2020/10/08.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+
+    @IBOutlet weak var prefecture1: UIButton!
+    @IBOutlet weak var prefecture2: UIButton!
+    @IBOutlet weak var prefecture3: UIButton!
+    @IBOutlet weak var prefecture4: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Kadai_09/ViewController.swift
+++ Kadai_09/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let initLabel = "未選択"
+    
+    @IBOutlet private weak var displayLabel: UILabel!
     override func viewDidLoad() {
+        self.displayLabel.text = initLabel
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func exitPrefecture1(segue:UIStoryboardSegue){
+        let second = segue.source as! SecondViewController
+        self.displayLabel.text = second.prefecture1.currentTitle
+    }
+    @IBAction func exitPrefecture2(segue:UIStoryboardSegue){
+        let second = segue.source as! SecondViewController
+        self.displayLabel.text = second.prefecture2.currentTitle
+    }
+    @IBAction func exitPrefecture3(segue:UIStoryboardSegue){
+        let second = segue.source as! SecondViewController
+        self.displayLabel.text = second.prefecture3.currentTitle
+    }
+    @IBAction func exitPrefecture4(segue:UIStoryboardSegue){
+        let second = segue.source as! SecondViewController
+        self.displayLabel.text = second.prefecture4.currentTitle
+    }
 
+    @IBAction func exitCancel(segue:UIStoryboardSegue){
+    }
 
 }
 
